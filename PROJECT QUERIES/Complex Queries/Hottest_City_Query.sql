ee5934f2a8f03f180a918e56dd368959c7811df6
Niv300000 <Niv300000@gmail.com>
2017-01-09 21:20:52 +0200

    Add files via upload

--- PROJECT QUERIES/Complex Queries/Hottest_City_Query.sql
+++ PROJECT QUERIES/Complex Queries/Hottest_City_Query.sql
@@ -1,5 +1,5 @@
-SELECT Place.city_id AS CityID,Event.id AS EventID
-FROM Event,Place, (SELECT City1.id AS CityId1 ,Count(Event1.id) As Num_Of_Events
+SELECT Event.*
+FROM Event,Place,(SELECT City1.id AS CityId1 ,Count(Event1.id) As Num_Of_Events
 								FROM Event AS Event1,Place AS Place1 ,City AS City1
 								WHERE City1.id=Place1.city_id and Place1.id=Event1.place_id
 								GROUP BY City1.id) AS CityEventsTable
@@ -7,4 +7,5 @@ WHERE Event.place_id=Place.id and Place.city_id=CityEventsTable.CityId1 and City
 																																					  	 FROM Event AS Event2,Place AS Place2 ,City AS City2
 																																					 	 WHERE City2.id=Place2.city_id and Place2.id=Event2.place_id
 																																					    GROUP BY City2.id)
-ORDER BY (Event.start_time);
+ORDER BY (Event.start_time)
+LIMIT 10;
--- PROJECT QUERIES/Complex Queries/Place with highest number of event from the category with highest number of attendings.sql
+++ PROJECT QUERIES/Complex Queries/Place with highest number of event from the category with highest number of attendings.sql
@@ -1,11 +1,13 @@
-SELECT Place.id, COUNT(Event.id) AS NumberOfEvents
-FROM Place,Event,(SELECT Category1.id AS CategoryID, SUM(Event1.attending_count) AS Num_Of_Attendings
-						FROM Event AS Event1 ,Category AS Category1
-						WHERE Event1.category_id=Category1.id
-						GROUP BY Category1.id
-						ORDER BY Num_Of_Attendings DESC
-						LIMIT 1) AS Category_With_Highest_Att
-WHERE Place.id=Event.place_id and Event.category_id=Category_With_Highest_Att.CategoryID
-GROUP BY Place.id
-ORDER BY(NumberOfEvents)
-LIMIT 1;
+SELECT Place.*
+FROM Place,(SELECT Place2.id AS PlaceID, COUNT(Event.id) AS NumberOfEvents
+				FROM Place AS Place2,Event,(SELECT Category1.id AS CategoryID, SUM(Event1.attending_count) AS Num_Of_Attendings
+										FROM Event AS Event1 ,Category AS Category1
+										WHERE Event1.category_id=Category1.id
+										GROUP BY Category1.id
+										ORDER BY Num_Of_Attendings DESC
+										LIMIT 1) AS Category_With_Highest_Att
+				WHERE Place2.id=Event.place_id and Event.category_id=Category_With_Highest_Att.CategoryID
+				GROUP BY Place2.id
+				ORDER BY(NumberOfEvents) DESC
+				LIMIT 1) AS table1
+WHERE Place.id=table1.PlaceID;
