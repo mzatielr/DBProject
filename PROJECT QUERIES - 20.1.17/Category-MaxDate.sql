515bda255ae10cc592fdbbd29e166524de54e96d
mzatielr <mordehaizai@gmail.com>
2017-01-21 13:00:04 +0200

    Update Category-MaxDate.sql

--- PROJECT QUERIES - 20.1.17/Category-MaxDate.sql
+++ PROJECT QUERIES - 20.1.17/Category-MaxDate.sql
@@ -1,5 +1,5 @@
 SELECT Cat1.id AS CategoryID,Cat1.name AS CategoryName, max(EventTime1.start_time) AS maxDate
 								FROM Category AS Cat1 ,Event AS Event1, Event_Time AS EventTime1
-								WHERE Cat1.id=Event1.category_id and Event.id=EventTime1.event_id
+								WHERE Cat1.id=Event1.category_id and Event1.id=EventTime1.event_id
 								GROUP BY CategoryID,CategoryName
 ORDER BY maxDate DESC;
